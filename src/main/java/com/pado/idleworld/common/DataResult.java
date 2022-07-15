8cadc3fcf2cb4d8f02f0c036cc504bc07d445071
pado0 <bgshhd95@gmail.com>
2022-07-15 15:02:43 +0900

    응답값 클래스 내 롬복 정리

--- src/main/java/com/pado/idleworld/common/DataResult.java
+++ src/main/java/com/pado/idleworld/common/DataResult.java
@@ -12,29 +12,3 @@ public class DataResult<T> extends CommonResult{
         super(responseCode);
     }
 }
-
-/* 사용법 */
-/*
-// 아래 코드와 같이 DataResult<응답클래스> 형태로 한 번 감싸서 리턴
-// todo는 적용 필요하지만 일단 아래와 같이 개발하는걸로 !
-
-    @PostMapping("/api/sign-up")
-    public DataResult<Account> signUp(@Valid @RequestBody SignUpForm signUpForm){
-
-
-        // todo : 비즈니스 로직에 의한 예외사항 발생, 이 로직 서비스 레이어로 내리기. Exception은 글로벌에서 처리
-        if(accountRepository.existsByEmail(signUpForm.getEmail()))
-            throw new AccountCannotCreateException(ExceptionCode.MEMBER_DUPLICATE);
-
-        Account account = accountService.createAccount(signUpForm);
-
-        // todo : DataResult 부분 생성자로 리팩토링하기
-        DataResult<Account> result = DataResult.<Account>builder()
-                .status(HttpStatus.OK)
-                .message("정상응답")
-                .data(account).build();
-
-        return result;
-    }
-
-* */
--- src/main/java/com/pado/idleworld/common/ResponseCode.java
+++ src/main/java/com/pado/idleworld/common/ResponseCode.java
@@ -1,20 +1,16 @@
 package com.pado.idleworld.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
 public enum ResponseCode {
 
     SUCCESS(200, "성공");
 
-    @Getter
     private int code;
-
-    @Getter
     private String message;
 
-    ResponseCode(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
 }
