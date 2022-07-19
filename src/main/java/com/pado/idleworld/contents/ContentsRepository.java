f5a0bf91fba8540779f46206278546ff1f9a835e
pado0 <bgshhd95@gmail.com>
2022-07-19 18:14:19 +0900

    베이스 카테고리 레퍼지토리 만드는 중, 재학님 merge

--- src/main/java/com/pado/idleworld/contents/ContentsRepository.java
+++ src/main/java/com/pado/idleworld/contents/ContentsRepository.java
@@ -2,6 +2,16 @@ package com.pado.idleworld.contents;
 
 import com.pado.idleworld.domain.Contents;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 public interface ContentsRepository extends JpaRepository<Contents, Long> {
+
+    @Query("select c from Contents c" +
+            " join fetch c.baseCategoryContents bc" +
+            " join fetch bc.baseCategory b")
+    public List<Contents> findAllByBaseCategoryId(Long categoryId);
+
 }
