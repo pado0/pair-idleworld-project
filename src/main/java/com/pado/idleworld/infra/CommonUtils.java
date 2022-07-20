2b70a14872fb768e2b014e0e485fd77bada5a4d7
pado0 <bgshhd95@gmail.com>
2022-07-20 15:53:35 +0900

    aws s3 이미지 업로드 테스트 완료

---
+++ src/main/java/com/pado/idleworld/infra/CommonUtils.java
@@ -0,0 +1,14 @@
+package com.pado.idleworld.infra;
+
+public class CommonUtils {
+    private static final String FILE_EXTENSION_SEPARATOR = ".";
+
+    public static String buildFileName(String category, String originalFileName) {
+        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
+        String fileExtension = originalFileName.substring(fileExtensionIndex);
+        String fileName = originalFileName.substring(0, fileExtensionIndex);
+        String now = String.valueOf(System.currentTimeMillis());
+
+        return category + fileName + now + fileExtension;
+    }
+}
